057d7e28e4329edabff6c54fda2dd00f807d4a12
Mauro Consiglio <Mauro@Mini-di-Mauro.lan>
2014-12-13 17:11:47 +0100

    added photo filters

---
+++ ExchangeAGram/ProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileViewController.swift
+//  ExchangeAGram
+//
+//  Created by Mauro Consiglio on 13/12/14.
+//  Copyright (c) 2014 Mauro Consiglio. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FeedItem.swift
+++ FeedItem.swift
@@ -2,7 +2,7 @@
 //  FeedItem.swift
 //  ExchangeAGram
 //
-//  Created by Mauro Consiglio on 11/12/14.
+//  Created by Mauro Consiglio on 13/12/14.
 //  Copyright (c) 2014 Mauro Consiglio. All rights reserved.
 //
 
@@ -10,9 +10,11 @@ import Foundation
 import CoreData
 
 @objc (FeedItem)
+
 class FeedItem: NSManagedObject {
 
     @NSManaged var caption: String
     @NSManaged var image: NSData
+    @NSManaged var thumbNail: NSData
 
 }
